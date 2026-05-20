d3ef2b2655a547bf1075d984cf20146586b738f2
Manasa <m.neruganti@gmail.com>
2026-05-19 20:58:17 -0600

    Added a create_list method

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+# from pydantic import BaseModel
 
 app = FastAPI()
+
+# e
+# class Item(BaseModel):
+    #text: str = None
+    #is_done: bool = False
 
 items = []
 
@@ -8,12 +14,21 @@ items = []
 def root():
     return {"Hello ": "World"}
 
-@app.post("/items")
-def create_item(item: str): 
-    items.append(item)
-    return items
+
+ @app.post("/items")
+ def create_item(item: str): 
+     items.append(item)
+     return items
+
+@app.get("/items")
+def list_items(limit: int = 10): # int = 10 is the default (return 10 items from the list)
+    return items[0:limit]
+
 
 @app.get("/items/{item_id}")
 def get_item(item_id: int) -> str:
-    item = items[item_id]
-    return item    
+    
+    if (item_id < len(items)):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail="Item not found")
